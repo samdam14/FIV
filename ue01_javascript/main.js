3d5f7f9da044e283c3bedb922b8167b8fafff42c
Daniel Sammer <samdam14@htlkaindorf.at>
2018-09-28 11:51:33 +0200

    Uebung 2

--- ue01_javascript/main.js
+++ ue01_javascript/main.js
@@ -1,6 +1,6 @@
 console.log('Guten Morgen');
-
 var jahr = 2018;
-let monat = 'September';
-
-console.log(monat + ' ' + jahr);
+var monat = 'September';
+var datum;
+datum = monat + jahr;
+console.log(datum);
